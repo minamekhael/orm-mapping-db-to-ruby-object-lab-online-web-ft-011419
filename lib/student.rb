6d89430b799970383afb7c559af648297049f689
Mina Mekhael <mina.mekhael@ymail.com>
2019-02-16 21:43:19 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -19,7 +19,16 @@ class Student
   end
 
   def self.find_by_name(name)
-    
+    sql = <<-SQL
+    FROM students
+    WHERE name = ?
+    LIMIT 1
+    SQL
+    DB[:conn].execute(sql,name).map do |row|
+      self.new_from_db(row)
+    end.first
+  end
+
   end
   
   def save
