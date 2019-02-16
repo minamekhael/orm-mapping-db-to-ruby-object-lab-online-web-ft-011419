4991561e4eeb04c6e66dfda8497e8ff3841fe2a5
Mina Mekhael <mina.mekhael@ymail.com>
2019-02-16 21:39:18 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,4 +1,7 @@
-  def self.new_from_db(row)
+class Student
+  attr_accessor :id, :name, :grade
+
+   def self.new_from_db(row)
     # create a new Student object given a row from the database
     new_student = self.new
     new_student.id = row[0]
@@ -52,24 +55,5 @@
 
   def save
     sql = <<-SQL
-      INSERT INTO students (name, grade) 
-      VALUES (?, ?)
-    SQL
-    DB[:conn].execute(sql, self.name, self.grade)
-  end
-  
-  def self.create_table
-    sql = <<-SQL
-    CREATE TABLE IF NOT EXISTS students (
-      id INTEGER PRIMARY KEY,
-      name TEXT,
-      grade TEXT
-    )
-    SQL
-    DB[:conn].execute(sql)
-  end
-  def self.drop_table
-    sql = "DROP TABLE IF EXISTS students"
-    DB[:conn].execute(sql)
-  end
-end
+      "INSERT INTO students (name, grade)"
+    end
